a2ddbdcb55e430f2b3df5dedf2ff5cca6a2c62d5
twb0223 <twb0223@gmail.com>
2018-08-31 17:13:25 +0800

    add cors

---
+++ src/main/java/com/twb/oauth/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.twb.oauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+    public String index(){
+        return "Index";
+    }
+}
